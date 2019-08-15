c3da53e438d40370ec90d50e8759865b7c3be949
Kylie Stoddard <kaiskidlets@gmail.com>
2019-08-15 14:28:44 -0600

    magic_8_ball #2 commit

---
+++ magic_8_ball.rb
@@ -0,0 +1,31 @@
+require "colorize"
+
+class Magic 
+  def initialize
+    menu
+  end
+
+  def menu 
+    puts "---Welcome to the Magic 8 Ball!---".colorize(:purple)
+    puts "1) Type 1 to ask a question and get an answer."
+    puts "2) Type 2 to end the program"
+    print ">"
+    choice = gets.to_i
+    case choice
+    when 1
+      puts "Ask Away!!"
+      print ">"
+      puts_(gets.strip)
+      menu 
+    when 2
+      exit
+    else 
+      puts "Invalid Input"
+      sleep (2)
+      print `clear`
+      menu
+    end
+  end
+end
+
+Magic.new
